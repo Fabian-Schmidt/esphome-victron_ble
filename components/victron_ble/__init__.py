88ac71cdcaf1b9e67d9ae1f107a6a50fdeb3c107
Your Name <you@example.com>
2023-02-22 16:09:49 +0100

    add esphome automation

--- components/victron_ble/__init__.py
+++ components/victron_ble/__init__.py
@@ -1,10 +1,12 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
+from esphome import automation
 from esphome.components import esp32_ble_tracker
 from esphome.const import (
     CONF_BINDKEY,
     CONF_ID,
     CONF_MAC_ADDRESS,
+    CONF_TRIGGER_ID,
 )
 from esphome.yaml_util import ESPHomeDumper
 
@@ -12,17 +14,95 @@ CODEOWNERS = ["@Fabian-Schmidt"]
 DEPENDENCIES = ["esp32_ble_tracker"]
 
 CONF_VICTRON_BLE_ID = "victron_ble_id"
+CONF_ON_BATTERY_MONITOR_MESSAGE = "on_battery_monitor_message"
+CONF_ON_SOLAR_CHARGER_MESSAGE = "on_solar_charger_message"
+CONF_ON_INVERTER_MESSAGE = "on_inverter_message"
+CONF_ON_DCDC_CONVERTER_MESSAGE = "on_dcdc_converter_message"
+CONF_ON_SMART_LITHIUM_MESSAGE = "on_smart_lithium_message"
+CONF_ON_INVERTER_RS_MESSAGE = "on_inverter_rs_message"
+CONF_ON_SMART_BATTERY_PROTECT_MESSAGE = "on_smart_battery_protect_message"
+CONF_ON_LYNX_SMART_BMS_MESSAGE = "on_lynx_smart_bms_message"
+CONF_ON_MULTI_RS_MESSAGE = "on_multi_rs_message"
+CONF_ON_VE_BUS_MESSAGE = "on_ve_bus_message"
+CONF_ON_DC_ENERGY_METER_MESSAGE = "on_dc_energy_meter_message"
+
+MULTI_CONF = True
 
 victron_ble_ns = cg.esphome_ns.namespace("victron_ble")
-VictronBle = victron_ble_ns.class_(
-    "VictronBle", esp32_ble_tracker.ESPBTDeviceListener, cg.PollingComponent
-)
-MULTI_CONF = True
+VictronBle = victron_ble_ns \
+    .class_("VictronBle", esp32_ble_tracker.ESPBTDeviceListener, cg.PollingComponent)
+
+BatteryMonitorMessageConstPtr = victron_ble_ns \
+    .struct("VICTRON_BLE_RECORD_BATTERY_MONITOR") \
+    .operator("ptr").operator("const")
+BatteryMonitorMessageTrigger = victron_ble_ns \
+    .class_("BatteryMonitorMessageTrigger", automation.Trigger.template(BatteryMonitorMessageConstPtr))
+
+SolarChargerMessageConstPtr = victron_ble_ns \
+    .struct("VICTRON_BLE_RECORD_SOLAR_CHARGER") \
+    .operator("ptr").operator("const")
+SolarChargerMessageTrigger = victron_ble_ns \
+    .class_("SolarChargerMessageTrigger", automation.Trigger.template(SolarChargerMessageConstPtr))
+
+InverterMessageConstPtr = victron_ble_ns \
+    .struct("VICTRON_BLE_RECORD_INVERTER") \
+    .operator("ptr").operator("const")
+InverterMessageTrigger = victron_ble_ns \
+    .class_("InverterMessageTrigger", automation.Trigger.template(InverterMessageConstPtr))
+
+DcDcConverterMessageConstPtr = victron_ble_ns \
+    .struct("VICTRON_BLE_RECORD_DCDC_CONVERTER") \
+    .operator("ptr").operator("const")
+DcDcConverterMessageTrigger = victron_ble_ns \
+    .class_("DcDcConverterMessageTrigger", automation.Trigger.template(DcDcConverterMessageConstPtr))
+
+SmartLithiumMessageConstPtr = victron_ble_ns \
+    .struct("VICTRON_BLE_RECORD_SMART_LITHIUM") \
+    .operator("ptr").operator("const")
+SmartLithiumMessageTrigger = victron_ble_ns \
+    .class_("SmartLithiumMessageTrigger", automation.Trigger.template(SmartLithiumMessageConstPtr))
+
+InverterRsMessageConstPtr = victron_ble_ns \
+    .struct("VICTRON_BLE_RECORD_INVERTER_RS") \
+    .operator("ptr").operator("const")
+InverterRsMessageTrigger = victron_ble_ns \
+    .class_("InverterRsMessageTrigger", automation.Trigger.template(InverterRsMessageConstPtr))
+
+SmartBatteryProtectMessageConstPtr = victron_ble_ns \
+    .struct("VICTRON_BLE_RECORD_SMART_BATTERY_PROTECT") \
+    .operator("ptr").operator("const")
+SmartBatteryProtectMessageTrigger = victron_ble_ns \
+    .class_("SmartBatteryProtectMessageTrigger", automation.Trigger.template(SmartBatteryProtectMessageConstPtr))
+
+LyncSmartBmsMessageConstPtr = victron_ble_ns \
+    .struct("VICTRON_BLE_RECORD_LYNX_SMART_BMS") \
+    .operator("ptr").operator("const")
+LyncSmartBmsMessageTrigger = victron_ble_ns \
+    .class_("LyncSmartBmsMessageTrigger", automation.Trigger.template(LyncSmartBmsMessageConstPtr))
+
+MultiRsMessageConstPtr = victron_ble_ns \
+    .struct("VICTRON_BLE_RECORD_MULTI_RS") \
+    .operator("ptr").operator("const")
+MultiRsMessageTrigger = victron_ble_ns \
+    .class_("MultiRsMessageTrigger", automation.Trigger.template(MultiRsMessageConstPtr))
+
+VeBusMessageConstPtr = victron_ble_ns \
+    .struct("VICTRON_BLE_RECORD_VE_BUS") \
+    .operator("ptr").operator("const")
+VeBusMessageTrigger = victron_ble_ns \
+    .class_("VeBusMessageTrigger", automation.Trigger.template(VeBusMessageConstPtr))
+
+DcEnergyMeterMessageConstPtr = victron_ble_ns \
+    .struct("VICTRON_BLE_RECORD_DC_ENERGY_METER") \
+    .operator("ptr").operator("const")
+DcEnergyMeterMessageTrigger = victron_ble_ns \
+    .class_("DcEnergyMeterMessageTrigger", automation.Trigger.template(DcEnergyMeterMessageConstPtr))
+
 
 class Array:
     def __init__(self, *parts):
         self.parts = parts
-    
+
     def __str__(self):
         return "".join(f"{part:02X}" for part in self.parts)
 
@@ -33,11 +113,13 @@ class Array:
         num = ", 0x".join(f"{part:02X}" for part in self.parts)
         return RawExpression(f"{{0x{num}}}")
 
+
 ESPHomeDumper.add_multi_representer(Array, ESPHomeDumper.represent_stringify)
+
 
 def bind_key_array(value):
     value = cv.bind_key(value)
-    parts = [value[i : i + 2] for i in range(0, len(value), 2)]
+    parts = [value[i: i + 2] for i in range(0, len(value), 2)]
     parts_int = []
     for part in parts:
         parts_int.append(int(part, 16))
@@ -52,11 +134,45 @@ CONFIG_SCHEMA = cv.All(
             cv.Required(CONF_MAC_ADDRESS): cv.mac_address,
             # TODO: Optional
             cv.Required(CONF_BINDKEY): bind_key_array,
+            cv.Optional(CONF_ON_BATTERY_MONITOR_MESSAGE): automation.validate_automation({
+                cv.GenerateID(CONF_TRIGGER_ID): cv.declare_id(BatteryMonitorMessageTrigger),
+            }),
+            cv.Optional(CONF_ON_SOLAR_CHARGER_MESSAGE): automation.validate_automation({
+                cv.GenerateID(CONF_TRIGGER_ID): cv.declare_id(SolarChargerMessageTrigger),
+            }),
+            cv.Optional(CONF_ON_INVERTER_MESSAGE): automation.validate_automation({
+                cv.GenerateID(CONF_TRIGGER_ID): cv.declare_id(InverterMessageTrigger),
+            }),
+            cv.Optional(CONF_ON_DCDC_CONVERTER_MESSAGE): automation.validate_automation({
+                cv.GenerateID(CONF_TRIGGER_ID): cv.declare_id(DcDcConverterMessageTrigger),
+            }),
+            cv.Optional(CONF_ON_SMART_LITHIUM_MESSAGE): automation.validate_automation({
+                cv.GenerateID(CONF_TRIGGER_ID): cv.declare_id(SmartLithiumMessageTrigger),
+            }),
+            cv.Optional(CONF_ON_INVERTER_RS_MESSAGE): automation.validate_automation({
+                cv.GenerateID(CONF_TRIGGER_ID): cv.declare_id(InverterRsMessageTrigger),
+            }),
+            cv.Optional(CONF_ON_SMART_BATTERY_PROTECT_MESSAGE): automation.validate_automation({
+                cv.GenerateID(CONF_TRIGGER_ID): cv.declare_id(SmartBatteryProtectMessageTrigger),
+            }),
+            cv.Optional(CONF_ON_LYNX_SMART_BMS_MESSAGE): automation.validate_automation({
+                cv.GenerateID(CONF_TRIGGER_ID): cv.declare_id(LyncSmartBmsMessageTrigger),
+            }),
+            cv.Optional(CONF_ON_MULTI_RS_MESSAGE): automation.validate_automation({
+                cv.GenerateID(CONF_TRIGGER_ID): cv.declare_id(MultiRsMessageTrigger),
+            }),
+            cv.Optional(CONF_ON_VE_BUS_MESSAGE): automation.validate_automation({
+                cv.GenerateID(CONF_TRIGGER_ID): cv.declare_id(VeBusMessageTrigger),
+            }),
+            cv.Optional(CONF_ON_DC_ENERGY_METER_MESSAGE): automation.validate_automation({
+                cv.GenerateID(CONF_TRIGGER_ID): cv.declare_id(DcEnergyMeterMessageTrigger),
+            }),
         }
     )
     .extend(esp32_ble_tracker.ESP_BLE_DEVICE_SCHEMA)
     .extend(cv.polling_component_schema("30s"))
 )
+
 
 async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
@@ -68,4 +184,46 @@ async def to_code(config):
     if CONF_BINDKEY in config:
         cg.add(var.set_bindkey(config[CONF_BINDKEY].as_array))
 
+    for conf in config.get(CONF_ON_BATTERY_MONITOR_MESSAGE, []):
+        trigger = cg.new_Pvariable(conf[CONF_TRIGGER_ID], var)
+        await automation.build_automation(trigger, [(BatteryMonitorMessageConstPtr, "message")], conf)
 
+    for conf in config.get(CONF_ON_SOLAR_CHARGER_MESSAGE, []):
+        trigger = cg.new_Pvariable(conf[CONF_TRIGGER_ID], var)
+        await automation.build_automation(trigger, [(SolarChargerMessageConstPtr, "message")], conf)
+
+    for conf in config.get(CONF_ON_INVERTER_MESSAGE, []):
+        trigger = cg.new_Pvariable(conf[CONF_TRIGGER_ID], var)
+        await automation.build_automation(trigger, [(InverterMessageConstPtr, "message")], conf)
+
+    for conf in config.get(CONF_ON_DCDC_CONVERTER_MESSAGE, []):
+        trigger = cg.new_Pvariable(conf[CONF_TRIGGER_ID], var)
+        await automation.build_automation(trigger, [(DcDcConverterMessageConstPtr, "message")], conf)
+
+    for conf in config.get(CONF_ON_SMART_LITHIUM_MESSAGE, []):
+        trigger = cg.new_Pvariable(conf[CONF_TRIGGER_ID], var)
+        await automation.build_automation(trigger, [(SmartLithiumMessageConstPtr, "message")], conf)
+
+    for conf in config.get(CONF_ON_INVERTER_RS_MESSAGE, []):
+        trigger = cg.new_Pvariable(conf[CONF_TRIGGER_ID], var)
+        await automation.build_automation(trigger, [(InverterRsMessageConstPtr, "message")], conf)
+
+    for conf in config.get(CONF_ON_SMART_BATTERY_PROTECT_MESSAGE, []):
+        trigger = cg.new_Pvariable(conf[CONF_TRIGGER_ID], var)
+        await automation.build_automation(trigger, [(SmartBatteryProtectMessageConstPtr, "message")], conf)
+
+    for conf in config.get(CONF_ON_LYNX_SMART_BMS_MESSAGE, []):
+        trigger = cg.new_Pvariable(conf[CONF_TRIGGER_ID], var)
+        await automation.build_automation(trigger, [(LyncSmartBmsMessageConstPtr, "message")], conf)
+
+    for conf in config.get(CONF_ON_MULTI_RS_MESSAGE, []):
+        trigger = cg.new_Pvariable(conf[CONF_TRIGGER_ID], var)
+        await automation.build_automation(trigger, [(MultiRsMessageConstPtr, "message")], conf)
+
+    for conf in config.get(CONF_ON_VE_BUS_MESSAGE, []):
+        trigger = cg.new_Pvariable(conf[CONF_TRIGGER_ID], var)
+        await automation.build_automation(trigger, [(VeBusMessageConstPtr, "message")], conf)
+
+    for conf in config.get(CONF_ON_DC_ENERGY_METER_MESSAGE, []):
+        trigger = cg.new_Pvariable(conf[CONF_TRIGGER_ID], var)
+        await automation.build_automation(trigger, [(DcEnergyMeterMessageConstPtr, "message")], conf)
---
+++ components/victron_ble/automation.h
@@ -0,0 +1,98 @@
+#pragma once
+
+#include "esphome/core/component.h"
+#include "victron_ble.h"
+
+namespace esphome {
+namespace victron_ble {
+
+class BatteryMonitorMessageTrigger : public Trigger<const VICTRON_BLE_RECORD_BATTERY_MONITOR *> {
+ public:
+  explicit BatteryMonitorMessageTrigger(VictronBle *parent) {
+    parent->add_on_battery_monitor_message_callback(
+        [this](const VICTRON_BLE_RECORD_BATTERY_MONITOR *message) { this->trigger(message); });
+  }
+};
+
+class SolarChargerMessageTrigger : public Trigger<const VICTRON_BLE_RECORD_SOLAR_CHARGER *> {
+ public:
+  explicit SolarChargerMessageTrigger(VictronBle *parent) {
+    parent->add_on_solar_charger_message_callback(
+        [this](const VICTRON_BLE_RECORD_SOLAR_CHARGER *message) { this->trigger(message); });
+  }
+};
+
+class InverterMessageTrigger : public Trigger<const VICTRON_BLE_RECORD_INVERTER *> {
+ public:
+  explicit InverterMessageTrigger(VictronBle *parent) {
+    parent->add_on_inverter_message_callback(
+        [this](const VICTRON_BLE_RECORD_INVERTER *message) { this->trigger(message); });
+  }
+};
+
+class DcDcConverterMessageTrigger : public Trigger<const VICTRON_BLE_RECORD_DCDC_CONVERTER *> {
+ public:
+  explicit DcDcConverterMessageTrigger(VictronBle *parent) {
+    parent->add_on_dcdc_converter_message_callback(
+        [this](const VICTRON_BLE_RECORD_DCDC_CONVERTER *message) { this->trigger(message); });
+  }
+};
+
+class SmartLithiumMessageTrigger : public Trigger<const VICTRON_BLE_RECORD_SMART_LITHIUM *> {
+ public:
+  explicit SmartLithiumMessageTrigger(VictronBle *parent) {
+    parent->add_on_smart_lithium_message_callback(
+        [this](const VICTRON_BLE_RECORD_SMART_LITHIUM *message) { this->trigger(message); });
+  }
+};
+
+class InverterRsMessageTrigger : public Trigger<const VICTRON_BLE_RECORD_INVERTER_RS *> {
+ public:
+  explicit InverterRsMessageTrigger(VictronBle *parent) {
+    parent->add_on_inverter_rs_message_callback(
+        [this](const VICTRON_BLE_RECORD_INVERTER_RS *message) { this->trigger(message); });
+  }
+};
+
+class SmartBatteryProtectMessageTrigger : public Trigger<const VICTRON_BLE_RECORD_SMART_BATTERY_PROTECT *> {
+ public:
+  explicit SmartBatteryProtectMessageTrigger(VictronBle *parent) {
+    parent->add_on_smart_battery_protect_message_callback(
+        [this](const VICTRON_BLE_RECORD_SMART_BATTERY_PROTECT *message) { this->trigger(message); });
+  }
+};
+
+class LyncSmartBmsMessageTrigger : public Trigger<const VICTRON_BLE_RECORD_LYNX_SMART_BMS *> {
+ public:
+  explicit LyncSmartBmsMessageTrigger(VictronBle *parent) {
+    parent->add_on_lynx_smart_bms_message_callback(
+        [this](const VICTRON_BLE_RECORD_LYNX_SMART_BMS *message) { this->trigger(message); });
+  }
+};
+
+class MultiRsMessageTrigger : public Trigger<const VICTRON_BLE_RECORD_MULTI_RS *> {
+ public:
+  explicit MultiRsMessageTrigger(VictronBle *parent) {
+    parent->add_on_multi_rs_message_callback(
+        [this](const VICTRON_BLE_RECORD_MULTI_RS *message) { this->trigger(message); });
+  }
+};
+
+class VeBusMessageTrigger : public Trigger<const VICTRON_BLE_RECORD_VE_BUS *> {
+ public:
+  explicit VeBusMessageTrigger(VictronBle *parent) {
+    parent->add_on_ve_bus_message_callback(
+        [this](const VICTRON_BLE_RECORD_VE_BUS *message) { this->trigger(message); });
+  }
+};
+
+class DcEnergyMeterMessageTrigger : public Trigger<const VICTRON_BLE_RECORD_DC_ENERGY_METER *> {
+ public:
+  explicit DcEnergyMeterMessageTrigger(VictronBle *parent) {
+    parent->add_on_dc_energy_meter_message_callback(
+        [this](const VICTRON_BLE_RECORD_DC_ENERGY_METER *message) { this->trigger(message); });
+  }
+};
+
+}  // namespace victron_ble
+}  // namespace esphome
